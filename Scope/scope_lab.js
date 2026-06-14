7361cc1c9d80b4233b6138ddebfa2ef65ff27e34
Dylan <dylanjohannroux1@gmail.com>
2026-06-14 04:31:57 -0400

    fix variable scope logging

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -24,11 +24,11 @@ function show(){
     var functionVar = "I'm a block-scoped var";
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
+
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
 }
 
 show();
-    
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
     
